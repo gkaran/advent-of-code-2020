cc721e02d7224d9c3edb9508318a66b2c32acafc
George Karanikas <gkaran89@gmail.com>
2020-12-08 10:43:09 +0200

    (chore) use joined arrays instead of multiline strings for test cases

--- day-06-custom-customs/day06-part1.spec.js
+++ day-06-custom-customs/day06-part1.spec.js
@@ -3,21 +3,24 @@ const part1 = require("./day06-part1");
 describe("Day 06: Custom Customs - Part 1", () => {
   test("solve README example", () =>
     expect(
-      part1(`
-abc
-
-a
-b
-c
-
-ab
-ac
-
-a
-a
-a
-a
-
-b`)
+      part1(
+        [
+          "abc",
+          "",
+          "a",
+          "b",
+          "c",
+          "",
+          "ab",
+          "ac",
+          "",
+          "a",
+          "a",
+          "a",
+          "a",
+          "",
+          "b",
+        ].join("\n")
+      )
     ).toBe(11));
 });
--- day-06-custom-customs/day06-part2.spec.js
+++ day-06-custom-customs/day06-part2.spec.js
@@ -3,20 +3,24 @@ const part2 = require("./day06-part2");
 describe("Day 06: Custom Customs - Part 2", () => {
   test("solve README example", () =>
     expect(
-      part2(`abc
-
-a
-b
-c
-
-ab
-ac
-
-a
-a
-a
-a
-
-b`)
+      part2(
+        [
+          "abc",
+          "",
+          "a",
+          "b",
+          "c",
+          "",
+          "ab",
+          "ac",
+          "",
+          "a",
+          "a",
+          "a",
+          "a",
+          "",
+          "b",
+        ].join("\n")
+      )
     ).toBe(6));
 });
