8c757fb260f6a1716d6360bcbe02d5b966271826
George Karanikas <gkaran89@gmail.com>
2020-12-13 01:09:12 +0200

    (feat) improve speed of Day 1 - Part 2

--- day-01-report-repair/day01-part2.js
+++ day-01-report-repair/day01-part2.js
@@ -1,17 +1,14 @@
-const _ = require("lodash");
-
 const reportRepair2 = (input) => {
-  const triplet = input
-    .map((v, i) =>
-      input
-        .slice(i + 1)
-        .map((w, j) => input.slice(j + 1).map((z) => [v, w, z]))
-        .flat()
-    )
-    .flat()
-    .filter((p) => _.sum(p) === 2020)[0];
-  if (!triplet) throw new Error("No triplet sums to 2020");
-  return _.reduce(triplet, _.multiply);
+  for (let i = 0; i < input.length - 2; i++) {
+    for (let j = 0; j < input.length - i - 1; j++) {
+      for (let k = 0; k < input.length - j; k++) {
+        if (input[i] + input[i + j] + input[i + j + k] === 2020)
+          return input[i] * input[i + j] * input[i + j + k];
+      }
+    }
+  }
+
+  throw new Error("No triplet sums to 2020");
 };
 
 module.exports = reportRepair2;
